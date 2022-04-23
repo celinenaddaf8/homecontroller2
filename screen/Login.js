dc5a1fd5b02bc6b5135d766ef6a0eb2f71e945e3
alijoumaa2 <97536893+alijoumaa2@users.noreply.github.com>
2022-04-23 14:00:28 +0300

    fixed error

--- screen/Login.js
+++ screen/Login.js
@@ -9,21 +9,21 @@ import {
   View,
   TextInput,
   TouchableOpacity,
-} from "react-native";
+} from 'react-native'
 import styled from 'styled-components/native'
 
 import Header from '../components/Header'
 import { auth, db } from '../firebase'
 
-import { AntDesign, Entypo } from "@expo/vector-icons";
+import { AntDesign, Entypo } from '@expo/vector-icons'
 
 const Container = styled.ScrollView`
   flex: 1;
-  background-color: white;
+  background-color: gray;
 `
 
 const FormWrapper = styled.KeyboardAvoidingView`
-  background-color: white;
+  background-color: gray;
   width: 100%;
   justify-content: center;
   align-items: center;
@@ -33,15 +33,12 @@ const FormWrapper = styled.KeyboardAvoidingView`
 const Form = styled.KeyboardAvoidingView`
   height: 400px;
   width: 90%;
-  background-color: white;;
+  background-color: orange;
   flex-direction: column;
   padding: 20px;
   justify-content: center;
   border-radius: 15px;
-  border:4px #f4a460;
-  
-  }
-`;
+`
 
 const SubmitForm = styled.TouchableOpacity`
   width: 95%;
@@ -51,9 +48,9 @@ const SubmitForm = styled.TouchableOpacity`
   justify-content: center;
   align-items: center;
   margin-top: 20px;
-  background-color: #f4a460;
+  background-color: grey;
   border-radius: 15px;
-`;
+`
 
 const StyledTextInput = styled.TextInput`
   background-color: white;
@@ -63,49 +60,40 @@ const StyledTextInput = styled.TextInput`
   font-size: 16px;
   border-radius: 15px;
   height: 60px;
-  margin-vertical: 3px;
+  margin-top: 3px;
   margin-bottom: 10px;
   color: gray;
-  border: 1px #ccc;
-`;
+`
 
 const StyledInputLabel = styled.Text`
   color: gray;
   font-size: 13px;
   text-align: left;
-`;
+`
 const LeftIcon = styled.View`
   left: 15px;
   top: 38px;
   position: absolute;
   z-index: 1;
-`;
+`
 
 const RightIcon = styled.TouchableOpacity`
   right: 15px;
   top: 38px;
   position: absolute;
   z-index: 1;
-`;
-
-const WelcomeText = styled.Text`
-  font-size: 16px;
-  color: gray;
-  margin: 10px;
-  text-align: left;
-  top:10px
-`;
+`
 
 const ButtonText = styled.Text`
   font-size: 15px;
   font-weight: bold;
   padding-left: 5px;
   color: white;
-`;
+`
 const SignInText = styled.Text`
   font-size: 30px;
   font-weight: bold;
-  color: #f4a460;
+  color: white;
   margin: 10px;
   text-align: left;
 `
@@ -118,7 +106,7 @@ const NewToApp = styled.Text`
   font-size: 15px;
   font-weight: 500;
   text-align: center;
-  color: gray;
+  color: #ccc;
   margin: 15px;
   text-align: center;
 `
@@ -132,8 +120,7 @@ const Login = ({ navigation }) => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [loading, setLoading] = useState(false)
-  const [hidePassword, setHidePassword ] = useState(true)
-  
+  const [hidePassword, setHidePassword] = useState(true)
 
   const login = () => {
     setLoading(true)
@@ -162,13 +149,12 @@ const Login = ({ navigation }) => {
 
   return (
     <>
-      <StatusBar style="white" />
+      <StatusBar style="grey" />
       <Container>
         <Overlay>
           <Header login={false} />
           <FormWrapper>
             <Form>
-              <WelcomeText>Welcome to Home Controller !</WelcomeText>
               <SignInText>Sign In</SignInText>
               <MyTextInput
                 label="Email Address"
@@ -192,11 +178,11 @@ const Login = ({ navigation }) => {
                 onChangeText={(text) => setPassword(text)}
               />
               <SubmitForm onPress={login} disabled={loading}>
-                <ButtonText>{loading ? "Loading..." : "Sign In"}</ButtonText>
+                <ButtonText>{loading ? 'Loading...' : 'Sign In'}</ButtonText>
               </SubmitForm>
               <NewToAppWrapper
                 activeOpacity={0.5}
-                onPress={() => navigation.navigate("Register")}
+                onPress={() => navigation.navigate('Register')}
               >
                 <NewToApp>New to our App ? Sign Up</NewToApp>
               </NewToAppWrapper>
@@ -205,7 +191,7 @@ const Login = ({ navigation }) => {
         </Overlay>
       </Container>
     </>
-  );
+  )
 }
 
 const MyTextInput = ({
@@ -214,26 +200,26 @@ const MyTextInput = ({
   icon,
   hidePassword,
   setHidePassword,
-  ...props}
-) => {
-  return(
-  <View>
-    <LeftIcon>
-      <AntDesign name={icon} size={24} color="grey" />
-    </LeftIcon>
-    <StyledInputLabel>{label}</StyledInputLabel>
-    <StyledTextInput {...props} />
-    {isPassword && (
-      <RightIcon onPress={() => setHidePassword (!hidePassword)}>
-        <Entypo
-          name={hidePassword ? "eye-with-line" : "eye"}
-          size={24}
-          color="grey"
-        />
-      </RightIcon>
-    )}
-  </View>);
-};
-
+  ...props
+}) => {
+  return (
+    <View>
+      <LeftIcon>
+        <AntDesign name={icon} size={24} color="grey" />
+      </LeftIcon>
+      <StyledInputLabel>{label}</StyledInputLabel>
+      <StyledTextInput {...props} />
+      {isPassword && (
+        <RightIcon onPress={() => setHidePassword(!hidePassword)}>
+          <Entypo
+            name={hidePassword ? 'eye-with-line' : 'eye'}
+            size={24}
+            color="grey"
+          />
+        </RightIcon>
+      )}
+    </View>
+  )
+}
 
 export default Login
