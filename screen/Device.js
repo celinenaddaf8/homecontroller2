873ae57736806ff0628bd10a43513776dd721b66
alijoumaa2 <97536893+alijoumaa2@users.noreply.github.com>
2022-05-01 09:41:23 +0300

    removing unesscary code

--- screen/Device.js
+++ screen/Device.js
@@ -1,36 +1,19 @@
-import React from "react";
-import { Text, Switch, View} from "react-native";
-import styled from "styled-components/native";
-import { useState } from "react";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { useNavigation } from "@react-navigation/native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { AntDesign } from "@expo/vector-icons";
+import React from 'react'
+import { Text, Switch, View } from 'react-native'
+import styled from 'styled-components/native'
+import { useState } from 'react'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { useNavigation } from '@react-navigation/native'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { AntDesign } from '@expo/vector-icons'
 
-const editButton = ({ uri, navigation }) => {
-  return (
-    <TouchableOpacity
-      onPress={() => {
-        navigation.navigate("Room");
-      }}
-      style={styles.appButtonContainer}
-    >
-      <Image
-        source={{
-          uri: uri,
-        }}
-        style={{ width: "100%", height: 200, borderRadius: 10 }}
-      ></Image>
-    </TouchableOpacity>
-  );
-};
 const Container = styled.View`
   flex: 1;
   width: 100%;
   height: 100%;
   align-items: center;
   justify-content: center;
-`;
+`
 
 const StyledCard = styled.View`
   height: 70%;
@@ -51,7 +34,7 @@ const StyledCard = styled.View`
   shadow-radius: 2;
   elevation: 1;
  
-`;
+`
 
 const EditButton = styled.TouchableOpacity`
   flex-direction: row;
@@ -63,41 +46,38 @@ const EditButton = styled.TouchableOpacity`
   elevation: 1;
   align-items: center;
   top:5%
-`;
+`
 
-
-
-const SwitchComp = ({id}) => {
-  const [isEnabled , setIsEnabled] = useState(true);
-  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+const SwitchComp = ({ id }) => {
+  const [isEnabled, setIsEnabled] = useState(true)
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState)
 
   return (
     <Switch
-      style={{ transform: [{ rotate: "-90deg" }] }}
-      trackColor={{ false: "gray", true: "#f4a460" }}
-      thumbColor={isEnabled ? "FFF" : "#f4f3f4"}
+      style={{ transform: [{ rotate: '-90deg' }] }}
+      trackColor={{ false: 'gray', true: '#f4a460' }}
+      thumbColor={isEnabled ? 'FFF' : '#f4f3f4'}
       ios_backgroundColor="#3e3e3e"
       onValueChange={toggleSwitch}
       value={isEnabled}
     />
-  );
-};
+  )
+}
 
 const Device = ({ deviceName }, id) => {
-  const navigation = useNavigation();
+  const navigation = useNavigation()
   return (
     <Container>
       <StyledCard>
         <MaterialCommunityIcons name="devices" size={44} color="gray" />
-        <Text style={{ color: "grey" }}>{deviceName}</Text>
+        <Text style={{ color: 'grey' }}>{deviceName}</Text>
         <SwitchComp></SwitchComp>
       </StyledCard>
-      <EditButton onPress={() => navigation.navigate("Monitor")}>
+      <EditButton onPress={() => navigation.navigate('Monitor')}>
         <AntDesign name="edit" size={24} color="gray" />
       </EditButton>
     </Container>
-  );
-};
- 
+  )
+}
 
-export default Device;
+export default Device
