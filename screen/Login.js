a46f4c5a19e4f767afa7cc02ae86d9a1d0ab9189
alijoumaa2 <97536893+alijoumaa2@users.noreply.github.com>
2022-05-01 09:49:57 +0300

    fixing errors

--- screen/Login.js
+++ screen/Login.js
@@ -9,13 +9,13 @@ import {
   View,
   TextInput,
   TouchableOpacity,
-} from "react-native";
+} from 'react-native'
 import styled from 'styled-components/native'
 
 import Header from '../components/Header'
 import { auth, db } from '../firebase'
 
-import { AntDesign, Entypo } from "@expo/vector-icons";
+import { AntDesign, Entypo } from '@expo/vector-icons'
 
 const Container = styled.ScrollView`
   flex: 1;
@@ -33,15 +33,13 @@ const FormWrapper = styled.KeyboardAvoidingView`
 const Form = styled.KeyboardAvoidingView`
   height: 90%;
   width: 90%;
-  background-color: white;;
+  background-color: white;
   flex-direction: column;
   padding: 20px;
   justify-content: center;
   border-radius: 15px;
-  border:4px #f4a460;
-  
-  }
-`;
+  border: 4px #f4a460;
+`
 
 const SubmitForm = styled.TouchableOpacity`
   width: 95%;
@@ -53,7 +51,7 @@ const SubmitForm = styled.TouchableOpacity`
   margin-top: 20px;
   background-color: #f4a460;
   border-radius: 15px;
-`;
+`
 
 const StyledTextInput = styled.TextInput`
   background-color: white;
@@ -63,45 +61,45 @@ const StyledTextInput = styled.TextInput`
   font-size: 16px;
   border-radius: 15px;
   height: 60px;
-  margin-vertical: 3px;
+  margin-top: 3px;
   margin-bottom: 10px;
   color: grey;
-  border:1px grey
-`;
+  border: 1px grey;
+`
 
 const StyledInputLabel = styled.Text`
   color: gray;
   font-size: 13px;
   text-align: left;
-`;
+`
 const LeftIcon = styled.View`
   left: 15px;
   top: 38px;
   position: absolute;
   z-index: 1;
-`;
+`
 
 const RightIcon = styled.TouchableOpacity`
   right: 15px;
   top: 38px;
   position: absolute;
   z-index: 1;
-`;
+`
 
 const WelcomeText = styled.Text`
   font-size: 16px;
   color: gray;
   margin: 10px;
   text-align: left;
-  top:10px
-`;
+  top: 10px;
+`
 
 const ButtonText = styled.Text`
   font-size: 15px;
   font-weight: bold;
   padding-left: 5px;
   color: white;
-`;
+`
 const SignInText = styled.Text`
   font-size: 30px;
   font-weight: bold;
@@ -131,66 +129,64 @@ const Overlay = styled.View`
 const MsgBox = styled.Text`
   text-align: center;
   font-size: 13px;
-  color:red
-`;
-
+  color: red;
+`
 
 const Login = ({ navigation }) => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [loading, setLoading] = useState(false)
-  const [hidePassword, setHidePassword ] = useState(true)
-  const [emailError, setEmailError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
-  
+  const [hidePassword, setHidePassword] = useState(true)
+  const [emailError, setEmailError] = useState('')
+  const [passwordError, setPasswordError] = useState('')
 
- const handleSubmit = () => {
-   setLoading(false)
-   var emailValid = false;
-   if (email.length == 0) {
-     setEmailError("Email is required");
-   } else if (email.length < 6) {
-     setEmailError("Email should be minimum 6 characters");
-   } else if (email.indexOf(" ") >= 0) {
-     setEmailError("Email cannot contain spaces");
-   } else if (email.indexOf("@") <= 0) {
-     setEmailError("Wrong email format ");
-   } else if (email.indexOf(".com") <= 0) {
-     setEmailError("Wrong email format");
-   } else {
-     emailValid = true;
-     setEmailError("");
-   }
+  const handleSubmit = () => {
+    setLoading(false)
+    var emailValid = false
+    if (email.length == 0) {
+      setEmailError('Email is required')
+    } else if (email.length < 6) {
+      setEmailError('Email should be minimum 6 characters')
+    } else if (email.indexOf(' ') >= 0) {
+      setEmailError('Email cannot contain spaces')
+    } else if (email.indexOf('@') <= 0) {
+      setEmailError('Wrong email format ')
+    } else if (email.indexOf('.com') <= 0) {
+      setEmailError('Wrong email format')
+    } else {
+      emailValid = true
+      setEmailError('')
+    }
 
-   var passValid = false;
-   if (password.length == 0) {
-     setPasswordError("Password is required");
-     setLoading(false);
-   } else if (password.length < 6) {
-     setPasswordError("Password should be minimum 6 characters");
-   } else if (password.indexOf(" ") >= 0) {
-     setPasswordError("Password cannot contain spaces");
-   } else {
-     passValid = true;
-     setPasswordError("");
-   }
-   if (emailValid && passValid){
-     setLoading(true)
-   };
-     auth
-       .signInWithEmailAndPassword(email, password)
-       .then((authUser) => {
-         navigation.replace("BottomStack");
-         setPassword("");
-         setEmail("");
-         console.log(authUser);
-         setLoading(false);
-       })
-       .catch((err) => {
-         setLoading(false);
-       });
- };
-  
+    var passValid = false
+    if (password.length == 0) {
+      setPasswordError('Password is required')
+      setLoading(false)
+    } else if (password.length < 6) {
+      setPasswordError('Password should be minimum 6 characters')
+    } else if (password.indexOf(' ') >= 0) {
+      setPasswordError('Password cannot contain spaces')
+    } else {
+      passValid = true
+      setPasswordError('')
+    }
+    if (emailValid && passValid) {
+      setLoading(true)
+    }
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        navigation.replace('BottomStack')
+        setPassword('')
+        setEmail('')
+        console.log(authUser)
+        setLoading(false)
+      })
+      .catch((err) => {
+        setLoading(false)
+      })
+  }
+
   return (
     <>
       <StatusBar style="white" />
@@ -226,11 +222,11 @@ const Login = ({ navigation }) => {
               {passwordError.length > 0 && <MsgBox>{passwordError}</MsgBox>}
 
               <SubmitForm onPress={handleSubmit} disabled={loading}>
-                <ButtonText>{loading ? "Loading..." : "Sign In"}</ButtonText>
+                <ButtonText>{loading ? 'Loading...' : 'Sign In'}</ButtonText>
               </SubmitForm>
               <NewToAppWrapper
                 activeOpacity={0.5}
-                onPress={() => navigation.navigate("Register")}
+                onPress={() => navigation.navigate('Register')}
               >
                 <NewToApp>New to our App ? Sign Up</NewToApp>
               </NewToAppWrapper>
@@ -239,7 +235,7 @@ const Login = ({ navigation }) => {
         </Overlay>
       </Container>
     </>
-  );
+  )
 }
 
 const MyTextInput = ({
@@ -248,26 +244,26 @@ const MyTextInput = ({
   icon,
   hidePassword,
   setHidePassword,
-  ...props}
-) => {
-  return(
-  <View>
-    <LeftIcon>
-      <AntDesign name={icon} size={24} color="grey" />
-    </LeftIcon>
-    <StyledInputLabel>{label}</StyledInputLabel>
-    <StyledTextInput {...props} />
-    {isPassword && (
-      <RightIcon onPress={() => setHidePassword (!hidePassword)}>
-        <Entypo
-          name={hidePassword ? "eye-with-line" : "eye"}
-          size={24}
-          color="grey"
-        />
-      </RightIcon>
-    )}
-  </View>);
-};
-
+  ...props
+}) => {
+  return (
+    <View>
+      <LeftIcon>
+        <AntDesign name={icon} size={24} color="grey" />
+      </LeftIcon>
+      <StyledInputLabel>{label}</StyledInputLabel>
+      <StyledTextInput {...props} />
+      {isPassword && (
+        <RightIcon onPress={() => setHidePassword(!hidePassword)}>
+          <Entypo
+            name={hidePassword ? 'eye-with-line' : 'eye'}
+            size={24}
+            color="grey"
+          />
+        </RightIcon>
+      )}
+    </View>
+  )
+}
 
 export default Login
