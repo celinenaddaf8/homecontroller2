2fa05669567c32c290bae37c07a8c9cde965b1e5
alijoumaa2 <97536893+alijoumaa2@users.noreply.github.com>
2022-05-02 11:49:12 +0300

    changing navigation

--- screen/Device.js
+++ screen/Device.js
@@ -1,12 +1,11 @@
-import React from "react";
-import { Text, Switch, View} from "react-native";
-import styled from "styled-components/native";
-import { useState } from "react";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { useNavigation } from "@react-navigation/native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { AntDesign } from "@expo/vector-icons";
-
+import React from 'react'
+import { Text, Switch, View } from 'react-native'
+import styled from 'styled-components/native'
+import { useState } from 'react'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { useNavigation } from '@react-navigation/native'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { AntDesign } from '@expo/vector-icons'
 
 const Container = styled.View`
   width: 55%;
@@ -14,7 +13,7 @@ const Container = styled.View`
   align-items: center;
   justify-content: center;
   flex-direction: column;
-`;
+`
 
 const StyledCard = styled.View`
   height: 70%;
@@ -25,50 +24,47 @@ const StyledCard = styled.View`
   border-width: 1.5;
   border-radius: 4;
   border-color: #ddd;
-`;
+`
 
 const EditButton = styled.TouchableOpacity`
-  width:30%
+  width: 30%;
   flex-direction: row;
   background-color: #f4a460;
-  border-radius:5;
+  border-radius: 5;
   justify-content: center;
-  top:5%
-`;
+  top: 5%;
+`
 
-
-
-const SwitchComp = ({id}) => {
-  const [isEnabled , setIsEnabled] = useState(true);
-  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+const SwitchComp = ({ id }) => {
+  const [isEnabled, setIsEnabled] = useState(true)
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState)
 
   return (
     <Switch
-      style={{ transform: [{ rotate: "-90deg" }] }}
-      trackColor={{ false: "gray", true: "#f4a460" }}
-      thumbColor={isEnabled ? "#EECC8C" : "#f5f5f5"}
+      style={{ transform: [{ rotate: '-90deg' }] }}
+      trackColor={{ false: 'gray', true: '#f4a460' }}
+      thumbColor={isEnabled ? '#EECC8C' : '#f5f5f5'}
       ios_backgroundColor="#3e3e3e"
       onValueChange={toggleSwitch}
       value={isEnabled}
     />
-  );
-};
+  )
+}
 
 const Device = ({ deviceName }, id) => {
-  const navigation = useNavigation();
+  const navigation = useNavigation()
   return (
     <Container>
       <StyledCard>
         <MaterialCommunityIcons name="devices" size={44} color="gray" />
-        <Text style={{ color: "grey" }}>{deviceName}</Text>
+        <Text style={{ color: 'grey' }}>{deviceName}</Text>
         <SwitchComp></SwitchComp>
       </StyledCard>
-      <EditButton onPress={() => navigation.navigate("Monitor")}>
+      <EditButton onPress={() => navigation.navigate('Edit')}>
         <AntDesign name="edit" size={24} color="gray" />
       </EditButton>
     </Container>
-  );
-};
- 
+  )
+}
 
-export default Device;
+export default Device
--- screen/Room.js
+++ screen/Room.js
@@ -2,15 +2,14 @@ import React from 'react'
 import { useState } from 'react'
 import { View, StyleSheet, Text, TextInput, Button, Switch } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
-import Device from './Device';
-import styled from 'styled-components/native';
-
+import Device from './Device'
+import styled from 'styled-components/native'
 
 const RoomContainer = styled.View`
-  width:100%
-  height:100%
-  background-color:white
-  `;
+  width: 100%;
+  height: 100%;
+  background-color: white;
+`
 
 const Container = styled.View`
   height: 25%;
@@ -19,30 +18,28 @@ const Container = styled.View`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-`;
+`
 const Container1 = styled.View`
   height: 50%;
   width: 100%;
   align-items: center;
-  justify-content:center;
+  justify-content: center;
   padding: 5%;
-  
-`;
+`
 
 const DeviceContainer = styled.View`
   height: 100%;
   width: 100%;
   border: 4px #f4a460;
   border-radius: 5px;
-`;
+`
 
 const Container2 = styled.View`
   align-items: center;
   height: 25%;
   width: 100%;
   display: flex;
-
-`;
+`
 const TextContainer = styled.View`
   height: 20%;
   width: 90%;
@@ -52,64 +49,62 @@ const TextContainer = styled.View`
   background-color: #f4a460;
   border-radius: 20px;
   top: 10%;
-`;
+`
 const TotalUsageText = styled.Text`
   font-size: 25;
   color: gray;
-`;
-
+`
 
 const Devices = styled.View`
   height: 50%;
   width: 100%;
   flex-direction: row;
   justify-content: space-evenly;
-  align-items:center; 
+  align-items: center;
   background-color: white;
-`;
-
+`
 
 const ReturnButton = styled.TouchableOpacity`
   background-color: #f4a460;
   color: grey;
   border-radius: 10px;
   border: none;
-  justify-content:center;
-  align-items:center;
+  justify-content: center;
+  align-items: center;
   flex-direction: row;
-  top:10%;
-  width:25%
-`;
+  top: 10%;
+  width: 25%;
+`
 const Logo2 = styled.Image`
   width: 20%;
   height: 30%;
   background-color: white;
-  top:30%
-`;
+  top: 30%;
+`
 
 const SwitchComp = () => {
-  const [isEnabled, setIsEnabled] = useState(true);
-  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+  const [isEnabled, setIsEnabled] = useState(true)
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState)
 
   return (
     <Switch
       style={{
-        transform: [{ scaleX: 1.4 }, { scaleY: 1.4 }, { rotate: "-90deg" }],
-        alignItems: "center",
+        transform: [{ scaleX: 1.4 }, { scaleY: 1.4 }, { rotate: '-90deg' }],
+        alignItems: 'center',
       }}
-      trackColor={{ false: "gray", true: "#f4a460" }}
-      thumbColor={isEnabled ? "#EECC8C" : "#f5f5f5"}
+      trackColor={{ false: 'gray', true: '#f4a460' }}
+      thumbColor={isEnabled ? '#EECC8C' : '#f5f5f5'}
       ios_backgroundColor="#3e3e3e"
       onValueChange={toggleSwitch}
       value={isEnabled}
     />
-  );
-};
+  )
+}
 const Room = ({ navigation }) => {
   return (
     <RoomContainer>
       <Container>
-        <Logo2 source={require("../assets/logoOrange.png")} />
+        <Logo2 source={require('../assets/logoOrange.png')} />
         <SwitchComp></SwitchComp>
       </Container>
       <Container1>
@@ -128,14 +123,14 @@ const Room = ({ navigation }) => {
         <TextContainer>
           <TotalUsageText>Total room usage: ... Amp </TotalUsageText>
         </TextContainer>
-        <ReturnButton onPress={() => navigation.navigate("Monitor")}>
-          <Text style={{ color: "grey", fontWeight: "bold", fontSize: 18 }}>
+        <ReturnButton onPress={() => navigation.navigate('Monitor')}>
+          <Text style={{ color: 'grey', fontWeight: 'bold', fontSize: 18 }}>
             return
           </Text>
         </ReturnButton>
       </Container2>
     </RoomContainer>
-  );
+  )
 }
 
 export default Room
